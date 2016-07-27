c415c11b37460d2791652f915d1962b172a678f4
sc3l <sean.z.chen@newegg.com>
2016-07-27 13:43:04 +0800

    Level 6. 使用 props 傳遞元件參數

--- app/TodoItem.js
+++ app/TodoItem.js
@@ -1,10 +1,16 @@
 class TodoItem extends React.Component {
 	render() {
-        <div>		
-            <input type="checkbox" /> 
-            <span>Item 1 </span>
-            <button> x </button>
-        </div>	
+        // 1. get param from this.props by parent component. 
+        const { title, completed } = this.props;
+
+        // 2. for reuse, render title & completed by param 
+        return (
+            <div>		
+                <input type="checkbox" checked={completed} /> 
+                <span>{title}</span>
+                <button>x</button>
+            </div>	
+        )
 	}
 }
 
--- app/TodoList.js
+++ app/TodoList.js
@@ -1,14 +1,23 @@
 class TodoList extends React.Component {
 	render() {
+
+        // set TodoItem param (title, completed).
+        // how to use: just like set html attribute. 
 		<ul>
             <li>
-            	<TodoItem />      
+            	<TodoItem 
+                    title="item 1"
+                    completed={true} />      
             </li>
             <li>
-            	<TodoItem />
+            	<TodoItem 
+                    title="item 2"
+                    completed={false}/>
             </li>
             <li>
-            	<TodoItem />
+            	<TodoItem 
+                    title="item 3"
+                    completed={false}/>
         	</li>
         </ul>
 	}
