f826ecc41a654c8e82199eee607da80fcb35c609
sc3l <sean.z.chen@newegg.com>
2016-07-29 15:14:29 +0800

    Level 10. 使用 React 表單元件 - 完成「新增項目」功能

--- app/InputField.js
+++ app/InputField.js
@@ -1,11 +1,41 @@
 class InputField extends React.Component {
+    constructor(props, context) {
+        super(props, context);
+        this.handleKeyDown = this.handleKeyDown.bind(this);
+    }
+
     render() {
         return (
-            <div>                
-                <input {...this.props} type="text" />
+            <div>
+                <input {...this.props}
+                    type="text"
+                    onKeyDown={this.handleKeyDown}
+                    />
             </div>
         );
     }
+
+    handleKeyDown(e) {
+        const {
+            onKeyDown,
+            onSubmitEditing
+        } = this.props;
+        const { value } = e.target;
+        switch (e.keyCode) {
+            case 13:
+                if (value.trim()) {
+                    // 如果使用者有輸入值，呼叫 callback 將 value 拋到上層元件。
+                    onSubmitEditing && onSubmitEditing(value);
+                }
+
+                e.target.value = '';
+                break;
+        }
+
+        onKeyDown && onKeyDown(e);
+    }
+
+
 }
 
 window.App.InputField = InputField;
--- app/TodoApp.js
+++ app/TodoApp.js
@@ -12,7 +12,7 @@ const demoTodos = [
         completed: false
     },
     {
-        id:1,
+        id: 1,
         title: 'Item 2',
         completed: true
     },
@@ -24,24 +24,31 @@ const demoTodos = [
 ];
 
 class TodoApp extends React.Component {
-    constructor(props, context){
+    constructor(props, context) {
         super(props, context);
         this.state = {
             todos: [...demoTodos]
         };
     }
-    
+
     render() {
         const { todos } = this.state;
         return (
             <div>
-                <TodoHeader 
+                <TodoHeader
                     title='我的代辦清單'
                     username='Sean'
-                    todoCount={todos.filter((todo) => !todo.completed).length} 
-                />
-                <InputField placeholder="新增代辦事項"/>
-                <TodoList 
+                    todoCount={todos.filter((todo) => !todo.completed).length}
+                    />
+                <InputField
+                    placeholder="新增代辦事項"
+                    onSubmitEditing={
+                        (title) => this.setState({
+                            todos: _createTodo(todos, title)
+                        })
+                    }
+                    />
+                <TodoList
                     todos={todos}
                     onDeleteTodo={
                         (id) => this.setState({
@@ -53,16 +60,16 @@ class TodoApp extends React.Component {
                             todos: _toggleTodo(todos, id, completed)
                         })
                     }
-                />
+                    />
             </div>
         );
-    }    
+    }
 }
 
 const _deleteTodo = (todos, id) => {
     const idx = todos.findIndex((todo) => todo.id === id);
     if (idx !== -1) {
-        todos.splice(idx, 1);            
+        todos.splice(idx, 1);
     }
 
     return todos;
@@ -73,6 +80,16 @@ const _toggleTodo = (todos, id, completed) => {
     if (target) {
         target.completed = completed;
     }
+
+    return todos;
+}
+
+const _createTodo = (todos, title) => {
+    todos.push({
+        id: todos[todos.length - 1].id + 1,
+        title,
+        completed: false
+    });
 
     return todos;
 }
