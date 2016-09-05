99a3a4af504630bf1edf6621768103590d6a13a9
sc3l <sean.z.chen@newegg.com>
2016-09-05 14:37:45 +0800

    完成 Actions：集中所有應用行為

---
+++ actions/TodoActions.js
@@ -0,0 +1,46 @@
+const {
+    ActionTypes,
+    AppDispatcher
+} = window.App;
+
+window.App.TodoActions = {
+    
+    loadTodos() {
+        fetch('http://localhost:3000/todos')
+            .then((response) => response.json())
+            .then((todos) => AppDispatcher.dispatch({
+                type: ActionTypes.LOAD_TODOS_SUCCESS,
+                todos
+            }));
+    },
+
+    createTodo(title) {
+        AppDispatcher.dispatch({
+            type: ActionTypes.CREATE_TODO,
+            title
+        });
+    },
+
+    updateTODO(id, title) {
+        AppDispatcher.dispatch({
+            type: ActionTypes.UPDATE_TODO,
+            id,
+            title
+        });
+    },
+
+    toggleTodo(id, completed) {
+        AppDispatcher.dispatch({
+            type: ActionTypes.TOGGLE_TODO,
+            id,
+            completed
+        });
+    },
+
+    deleteTodo(id) {
+        AppDispatcher.dispatch({
+            type: ActionTypes.DELETE_TODO,
+            id
+        });
+    }
+}
